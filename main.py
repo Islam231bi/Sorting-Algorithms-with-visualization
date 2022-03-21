20116f60d5d00457018dedfb5dcff03dcb78b796
islam231bi <islambassuni@gmail.com>
2022-03-21 17:43:36 +0200

    modified merge sort and working

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from selectionSort import SelectionSort
 from BubbleSort import BubbleSort
 from inersionSort import insertionSort
 
-list1 = [1, 8, 4]
+list1 = [1, 8, 4,23,0,2,3,10,9,11]
 
 sort = MergeSort(list1)
